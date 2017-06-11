a2e9025f64801026cecfad2cfee0af6b2a20ba57
Aaron Kling <webgeek1234@gmail.com>
2017-06-11 16:55:10 -0500

    Update rm-wrapper init sequence

--- rm-wrapper/Android.mk
+++ rm-wrapper/Android.mk
@@ -3,8 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := rm-wrapper
 LOCAL_SRC_FILES := rm-wrapper.c
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_CFLAGS := -fPIE
-LOCAL_LDFLAGS := -fPIE -pie -ldl -llog
+LOCAL_LDFLAGS := -fPIE -pie -ldl -llog -Wl,-dynamic-linker,/sbin/linker
 LOCAL_C_INCLUDES := $(ANDROID_BUILD_TOP)/bionic/libc/stdio
 LOCAL_SHARED_LIBRARIES := libc libdl liblog libm libstdc++
 include $(BUILD_EXECUTABLE)
