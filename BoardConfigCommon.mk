a62bc6c0d84be4d72a30ee7b621631902777ad06
Lolzen <funer@gmx.at>
2018-02-19 22:09:18 +0100

    add omni sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,7 +16,8 @@
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/nvidia/shield-common/sepolicy/common \
-                       device/nvidia/shield-common/sepolicy/lineage-common
+                       device/nvidia/shield-common/sepolicy/lineage-common \
+                       vendor/omni/sepolicy
 
 ifneq ($(filter $(TARGET_TEGRA_MODEM),icera),)
 	BOARD_SEPOLICY_DIRS += device/nvidia/shield-common/sepolicy/icera
